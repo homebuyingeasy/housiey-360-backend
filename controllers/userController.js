795fe5e65441538e3b3911171ba120166fabf792
himadri-housiey <himadri.bar@housiey.com>
2024-09-21 17:38:57 +0530

    updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -188,7 +188,7 @@ exports.forgotPassword = async (req, res) => {
             subject: 'Password Reset',
             text: `You are receiving this because you have requested the reset of the password for your account.\n\n
           Please click on the following link, or paste this into your browser to complete the process:\n\n
-          http://${process.env.RESET_PASSWORD_LINK}${resetToken}\n\n
+          ${process.env.RESET_PASSWORD_LINK}${resetToken}\n\n
           If you did not request this, please ignore this email and your password will remain unchanged.\n`
         };
         transporter.sendMail(mailOptions);
